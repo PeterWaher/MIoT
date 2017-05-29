626d6178a87ee72f6bc1f2152398f1935ec683e7
Peter Waher <peterwaher@hotmail.com>
2017-05-29 14:45:30 +0200

    Adding relay control.

--- Actuator/App.xaml.cs
+++ Actuator/App.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,86 +18,198 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.Devices.Enumeration;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using Waher.Events;
+using Waher.Persistence;
+using Waher.Persistence.Files;
+using Waher.Persistence.Filters;
+using Waher.Script;
+
 namespace Actuator
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		private static App instance = null;
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
-            Frame rootFrame = Window.Current.Content as Frame;
+		private UsbSerial arduinoUsb = null;
+		private RemoteDevice arduino = null;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+			Frame rootFrame = Window.Current.Content as Frame;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
 
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
+				rootFrame.NavigationFailed += OnNavigationFailed;
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
-        }
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Load state from previously suspended application
+				}
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
-    }
+			if (e.PrelaunchActivated == false)
+			{
+				if (rootFrame.Content == null)
+				{
+					// When the navigation stack isn't restored navigate to the first page,
+					// configuring the new page by passing required information as a navigation
+					// parameter
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				}
+				// Ensure the current window is active
+				instance = this;
+				Window.Current.Activate();
+				Task.Run((Action)this.Init);
+			}
+		}
+
+		private async void Init()
+		{
+			try
+			{
+				Log.Informational("Starting application.");
+				Types.Initialize(typeof(FilesProvider).GetTypeInfo().Assembly, typeof(App).GetTypeInfo().Assembly);
+				Database.Register(new FilesProvider(Windows.Storage.ApplicationData.Current.LocalFolder.Path +
+					Path.DirectorySeparatorChar + "Data", "Default", 8192, 1000, 8192, Encoding.UTF8, 10000));
+
+				DeviceInformationCollection Devices = await UsbSerial.listAvailableDevicesAsync();
+				foreach (DeviceInformation DeviceInfo in Devices)
+				{
+					if (DeviceInfo.IsEnabled && DeviceInfo.Name.StartsWith("Arduino"))
+					{
+						Log.Informational("Connecting to " + DeviceInfo.Name);
+
+						this.arduinoUsb = new UsbSerial(DeviceInfo);
+						this.arduinoUsb.ConnectionEstablished += () =>
+							Log.Informational("USB connection established.");
+
+						this.arduino = new RemoteDevice(this.arduinoUsb);
+						this.arduino.DeviceReady += () =>
+						{
+							Log.Informational("Device ready.");
+
+							this.arduino.pinMode(13, PinMode.OUTPUT);    // Onboard LED.
+							this.arduino.digitalWrite(13, PinState.HIGH);
+
+							this.arduino.pinMode(0, PinMode.INPUT);      // PIR sensor (motion detection).
+
+							this.arduino.pinMode(1, PinMode.OUTPUT);     // Relay.
+							this.arduino.digitalWrite(1, 0);             // Relay set to 0
+
+							this.arduino.pinMode("A0", PinMode.ANALOG); // Light sensor.
+						};
+
+						this.arduinoUsb.ConnectionFailed += message =>
+						{
+							Log.Error("USB connection failed: " + message);
+						};
+
+						this.arduinoUsb.ConnectionLost += message =>
+						{
+							Log.Error("USB connection lost: " + message);
+						};
+
+						this.arduinoUsb.begin(57600, SerialConfig.SERIAL_8N1);
+						break;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Emergency(ex);
+
+				MessageDialog Dialog = new MessageDialog(ex.Message, "Error");
+				await Dialog.ShowAsync();
+			}
+		}
+
+		internal static App Instance => instance;
+
+		internal void TurnRelay(bool On, string Actor)
+		{
+			if (this.arduino != null)
+			{
+				this.arduino.digitalWrite(1, On ? PinState.HIGH : PinState.LOW);
+
+				Log.Informational("Turning relay " + (On ? "ON" : "OFF") + ".", string.Empty);
+
+
+			}
+		}
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+
+			if (instance == this)
+				instance = null;
+
+			if (this.arduino != null)
+			{
+				this.arduino.digitalWrite(13, PinState.LOW);
+				this.arduino.pinMode(13, PinMode.INPUT);     // Onboard LED.
+				this.arduino.pinMode(1, PinMode.INPUT);      // Relay.
+
+				this.arduino.Dispose();
+				this.arduino = null;
+			}
+
+			if (this.arduinoUsb != null)
+			{
+				this.arduinoUsb.end();
+				this.arduinoUsb.Dispose();
+				this.arduinoUsb = null;
+			}
+
+			Log.Terminate();
+
+			deferral.Complete();
+		}
+	}
 }
--- Actuator/MainPage.xaml.cs
+++ Actuator/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Waher.Events;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,62 @@ namespace Actuator
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+		private static MainPage instance = null;
+		private Events events;
+
+		public MainPage()
         {
             this.InitializeComponent();
-        }
-    }
+
+			this.events = new MainPage.Events();
+			Log.Register(this.events);
+
+			if (instance == null)
+				instance = this;
+		}
+
+		private void Page_Unloaded(object sender, RoutedEventArgs e)
+		{
+			Log.Unregister(this.events);
+			this.events = null;
+
+			if (instance == this)
+				instance = null;
+		}
+
+		public static MainPage Instance
+		{
+			get { return instance; }
+		}
+
+		public async void AddLogMessage(string Message)
+		{
+			DateTime TP = DateTime.Now;
+
+			await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				this.EventsPanel.Children.Insert(0, new TextBlock() { Text = Message });
+
+				while (this.EventsPanel.Children.Count > 1000)
+					this.EventsPanel.Children.RemoveAt(1000);
+			});
+		}
+
+		private class Events : EventSink
+		{
+			public Events() : base(string.Empty)
+			{
+			}
+
+			public override void Queue(Event Event)
+			{
+				MainPage.instance.AddLogMessage(Event.Message);
+			}
+		}
+
+		private void Relay_Click(object sender, RoutedEventArgs e)
+		{
+			App.Instance.TurnRelay(this.Relay.IsChecked.HasValue && this.Relay.IsChecked.Value, "Windows user");
+		}
+	}
 }
