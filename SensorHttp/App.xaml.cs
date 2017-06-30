1d03827af606ce9a096abd8636cb877b7745c089
Peter Waher <peterwaher@hotmail.com>
2017-06-30 13:43:03 +0200

    Adding SensorHttp project.

---
+++ SensorHttp/App.xaml.cs
@@ -0,0 +1,554 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Devices.Enumeration;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using Waher.Events;
+using Waher.Networking;
+using Waher.Networking.HTTP;
+using Waher.Networking.Sniffers;
+using Waher.Persistence;
+using Waher.Persistence.Files;
+using Waher.Persistence.Filters;
+using Waher.Runtime.Inventory;
+using Waher.Runtime.Settings;
+
+using SensorHttp.History;
+
+namespace SensorHttp
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		private UsbSerial arduinoUsb = null;
+		private RemoteDevice arduino = null;
+		private Timer sampleTimer = null;
+		private HttpServer httpServer = null;
+
+		private const int windowSize = 10;
+		private const int spikePos = windowSize / 2;
+		private int?[] windowA0 = new int?[windowSize];
+		private int nrA0 = 0;
+		private int sumA0 = 0;
+
+		private int? lastMinute = null;
+		private double? minLight = null;
+		private double? maxLight = null;
+		private double sumLight = 0;
+		private int sumMovement = 0;
+		private int nrTerms = 0;
+		private DateTime minLightAt = DateTime.MinValue;
+		private DateTime maxLightAt = DateTime.MinValue;
+		private string deviceId;
+		private double? lastLight = null;
+		private bool? lastMovement = null;
+		private double? lastPublishedLight = null;
+		private bool? lastPublishedMovement = null;
+		private DateTime lastLightPublishTime = DateTime.MinValue;
+		private DateTime lastMovementPublishTime = DateTime.MinValue;
+
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+			Frame rootFrame = Window.Current.Content as Frame;
+
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame();
+
+				rootFrame.NavigationFailed += OnNavigationFailed;
+
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Load state from previously suspended application
+				}
+
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
+
+			if (e.PrelaunchActivated == false)
+			{
+				if (rootFrame.Content == null)
+				{
+					// When the navigation stack isn't restored navigate to the first page,
+					// configuring the new page by passing required information as a navigation
+					// parameter
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				}
+				// Ensure the current window is active
+				Window.Current.Activate();
+				Task.Run((Action)this.Init);
+			}
+		}
+
+		private async void Init()
+		{
+			try
+			{
+				Log.Informational("Starting application.");
+
+				Types.Initialize(
+					typeof(FilesProvider).GetTypeInfo().Assembly,
+					typeof(RuntimeSettings).GetTypeInfo().Assembly,
+					typeof(App).GetTypeInfo().Assembly);
+
+				Database.Register(new FilesProvider(Windows.Storage.ApplicationData.Current.LocalFolder.Path +
+					Path.DirectorySeparatorChar + "Data", "Default", 8192, 1000, 8192, Encoding.UTF8, 10000));
+
+				DeviceInformationCollection Devices = await UsbSerial.listAvailableDevicesAsync();
+				foreach (DeviceInformation DeviceInfo in Devices)
+				{
+					if (DeviceInfo.IsEnabled && DeviceInfo.Name.StartsWith("Arduino"))
+					{
+						Log.Informational("Connecting to " + DeviceInfo.Name);
+
+						this.arduinoUsb = new UsbSerial(DeviceInfo);
+						this.arduinoUsb.ConnectionEstablished += () =>
+							Log.Informational("USB connection established.");
+
+						this.arduino = new RemoteDevice(this.arduinoUsb);
+						this.arduino.DeviceReady += () =>
+						{
+							Log.Informational("Device ready.");
+
+							this.arduino.pinMode(13, PinMode.OUTPUT);    // Onboard LED.
+							this.arduino.digitalWrite(13, PinState.HIGH);
+
+							this.arduino.pinMode(0, PinMode.INPUT);      // PIR sensor (motion detection).
+							MainPage.Instance.DigitalPinUpdated(0, this.arduino.digitalRead(0));
+
+							this.arduino.pinMode(1, PinMode.OUTPUT);     // Relay.
+							this.arduino.digitalWrite(1, 0);             // Relay set to 0
+
+							this.arduino.pinMode("A0", PinMode.ANALOG); // Light sensor.
+							MainPage.Instance.AnalogPinUpdated("A0", this.arduino.analogRead("A0"));
+
+							this.sampleTimer = new Timer(this.SampleValues, null, 1000 - DateTime.Now.Millisecond, 1000);
+						};
+
+						this.arduino.AnalogPinUpdated += (pin, value) =>
+						{
+							MainPage.Instance.AnalogPinUpdated(pin, value);
+						};
+
+						this.arduino.DigitalPinUpdated += (pin, value) =>
+						{
+							MainPage.Instance.DigitalPinUpdated(pin, value);
+						};
+
+						this.arduinoUsb.ConnectionFailed += message =>
+						{
+							Log.Error("USB connection failed: " + message);
+						};
+
+						this.arduinoUsb.ConnectionLost += message =>
+						{
+							Log.Error("USB connection lost: " + message);
+						};
+
+						this.arduinoUsb.begin(57600, SerialConfig.SERIAL_8N1);
+						break;
+					}
+				}
+
+				this.deviceId = await RuntimeSettings.GetAsync("DeviceId", string.Empty);
+				if (string.IsNullOrEmpty(this.deviceId))
+				{
+					this.deviceId = Guid.NewGuid().ToString().Replace("-", string.Empty);
+					await RuntimeSettings.SetAsync("DeviceId", this.deviceId);
+				}
+
+				Log.Informational("Device ID: " + this.deviceId);
+
+				//this.httpServer = new HttpServer();
+
+				this.httpServer = new HttpServer(new LogSniffer());
+
+				this.httpServer.Register("/Hello", (req, resp) =>
+				{
+					resp.ContentType = "text/plain";
+					resp.Write("World!");
+				});
+
+				this.httpServer.Register("/Light", (req, resp) =>
+				{
+					if (req.Header.Accept == null ||
+						req.Header.Accept.IsAcceptable("text/xml") ||
+						req.Header.Accept.IsAcceptable("application/xml"))
+					{
+						resp.ContentType = "text/plain";
+						resp.Write("Hello");
+					}
+					else if (req.Header.Accept.IsAcceptable("application/json"))
+					{
+					}
+					else if (req.Header.Accept.IsAcceptable("image/png"))
+					{
+					}
+					else
+						throw new NotAcceptableException();
+				});
+			}
+			catch (Exception ex)
+			{
+				Log.Emergency(ex);
+
+				MessageDialog Dialog = new MessageDialog(ex.Message, "Error");
+				await Dialog.ShowAsync();
+			}
+		}
+
+		private async void SampleValues(object State)
+		{
+			try
+			{
+				DateTime Timestamp = DateTime.Now;
+				ushort A0 = this.arduino.analogRead("A0");
+				PinState D0 = this.arduino.digitalRead(0);
+
+				if (this.windowA0[0].HasValue)
+				{
+					this.sumA0 -= this.windowA0[0].Value;
+					this.nrA0--;
+				}
+
+				Array.Copy(this.windowA0, 1, this.windowA0, 0, windowSize - 1);
+				this.windowA0[windowSize - 1] = A0;
+				this.sumA0 += A0;
+				this.nrA0++;
+
+				double AvgA0 = ((double)this.sumA0) / this.nrA0;
+				int? v;
+
+				if (this.nrA0 >= windowSize - 2)
+				{
+					int NrLt = 0;
+					int NrGt = 0;
+
+					foreach (int? Value in this.windowA0)
+					{
+						if (Value.HasValue)
+						{
+							if (Value.Value < AvgA0)
+								NrLt++;
+							else if (Value.Value > AvgA0)
+								NrGt++;
+						}
+					}
+
+					if (NrLt == 1 || NrGt == 1)
+					{
+						v = this.windowA0[spikePos];
+
+						if (v.HasValue)
+						{
+							if ((NrLt == 1 && v.Value < AvgA0) || (NrGt == 1 && v.Value > AvgA0))
+							{
+								this.sumA0 -= v.Value;
+								this.nrA0--;
+								this.windowA0[spikePos] = null;
+
+								AvgA0 = ((double)this.sumA0) / this.nrA0;
+
+								Log.Informational("Spike removed.", new KeyValuePair<string, object>("A0", v.Value));
+							}
+						}
+					}
+				}
+
+				int i, n;
+
+				for (AvgA0 = i = n = 0; i < spikePos; i++)
+				{
+					if ((v = this.windowA0[i]).HasValue)
+					{
+						n++;
+						AvgA0 += v.Value;
+					}
+				}
+
+				if (n > 0)
+				{
+					AvgA0 /= n;
+					double Light = (100.0 * AvgA0) / 1024;
+					MainPage.Instance.LightUpdated(Light, 2, "%");
+
+					this.sumLight += Light;
+					this.sumMovement += (D0 == PinState.HIGH ? 1 : 0);
+					this.nrTerms++;
+
+					if (!this.minLight.HasValue || Light < this.minLight.Value)
+					{
+						this.minLight = Light;
+						this.minLightAt = Timestamp;
+					}
+
+					if (!this.maxLight.HasValue || Light > this.maxLight.Value)
+					{
+						this.maxLight = Light;
+						this.maxLightAt = Timestamp;
+					}
+
+					if (!this.lastMinute.HasValue)
+						this.lastMinute = Timestamp.Minute;
+					else if (this.lastMinute.Value != Timestamp.Minute)
+					{
+						this.lastMinute = Timestamp.Minute;
+
+						LastMinute Rec = new LastMinute()
+						{
+							Timestamp = Timestamp,
+							Light = Light,
+							Movement = D0,
+							MinLight = this.minLight,
+							MinLightAt = this.minLightAt,
+							MaxLight = this.maxLight,
+							MaxLightAt = this.maxLightAt,
+							AvgLight = (this.nrTerms == 0 ? (double?)null : this.sumLight / this.nrTerms),
+							AvgMovement = (this.nrTerms == 0 ? (double?)null : this.sumMovement / this.nrTerms)
+						};
+
+						await Database.Insert(Rec);
+
+						this.minLight = null;
+						this.minLightAt = DateTime.MinValue;
+						this.maxLight = null;
+						this.maxLightAt = DateTime.MinValue;
+						this.sumLight = 0;
+						this.sumMovement = 0;
+						this.nrTerms = 0;
+
+						foreach (LastMinute Rec2 in await Database.Find<LastMinute>(new FilterFieldLesserThan("Timestamp", Timestamp.AddMinutes(-100))))
+							await Database.Delete(Rec2);
+
+						if (Timestamp.Minute == 0)
+						{
+							DateTime From = new DateTime(Timestamp.Year, Timestamp.Month, Timestamp.Day, Timestamp.Hour, 0, 0).AddHours(-1);
+							DateTime To = From.AddHours(1);
+							int NLight = 0;
+							int NMovement = 0;
+
+							LastHour HourRec = new LastHour()
+							{
+								Timestamp = Timestamp,
+								Light = Light,
+								Movement = D0,
+								MinLight = Rec.MinLight,
+								MinLightAt = Rec.MinLightAt,
+								MaxLight = Rec.MaxLight,
+								MaxLightAt = Rec.MaxLightAt,
+								AvgLight = 0,
+								AvgMovement = 0
+							};
+
+							foreach (LastMinute Rec2 in await Database.Find<LastMinute>(0, 60, new FilterAnd(
+								new FilterFieldLesserThan("Timestamp", To),
+								new FilterFieldGreaterOrEqualTo("Timestamp", From))))
+							{
+								if (Rec2.AvgLight.HasValue)
+								{
+									HourRec.AvgLight += Rec2.AvgLight.Value;
+									NLight++;
+								}
+
+								if (Rec2.AvgMovement.HasValue)
+								{
+									HourRec.AvgMovement += Rec2.AvgMovement.Value;
+									NMovement++;
+								}
+
+								if (Rec2.MinLight < HourRec.MinLight)
+								{
+									HourRec.MinLight = Rec2.MinLight;
+									HourRec.MinLightAt = Rec.MinLightAt;
+								}
+
+								if (Rec2.MaxLight < HourRec.MaxLight)
+								{
+									HourRec.MaxLight = Rec2.MaxLight;
+									HourRec.MaxLightAt = Rec.MaxLightAt;
+								}
+							}
+
+							if (NLight == 0)
+								HourRec.AvgLight = null;
+							else
+								HourRec.AvgLight /= NLight;
+
+							if (NMovement == 0)
+								HourRec.AvgMovement = null;
+							else
+								HourRec.AvgMovement /= NMovement;
+
+							await Database.Insert(HourRec);
+
+							foreach (LastHour Rec2 in await Database.Find<LastHour>(new FilterFieldLesserThan("Timestamp", Timestamp.AddHours(-100))))
+								await Database.Delete(Rec2);
+
+							if (Timestamp.Hour == 0)
+							{
+								From = new DateTime(Timestamp.Year, Timestamp.Month, Timestamp.Day, 0, 0, 0).AddDays(-1);
+								To = From.AddDays(1);
+								NLight = 0;
+								NMovement = 0;
+
+								LastDay DayRec = new LastDay()
+								{
+									Timestamp = Timestamp,
+									Light = Light,
+									Movement = D0,
+									MinLight = HourRec.MinLight,
+									MinLightAt = HourRec.MinLightAt,
+									MaxLight = HourRec.MaxLight,
+									MaxLightAt = HourRec.MaxLightAt,
+									AvgLight = 0,
+									AvgMovement = 0
+								};
+
+								foreach (LastHour Rec2 in await Database.Find<LastHour>(0, 24, new FilterAnd(
+									new FilterFieldLesserThan("Timestamp", To),
+									new FilterFieldGreaterOrEqualTo("Timestamp", From))))
+								{
+									if (Rec2.AvgLight.HasValue)
+									{
+										DayRec.AvgLight += Rec2.AvgLight.Value;
+										NLight++;
+									}
+
+									if (Rec2.AvgMovement.HasValue)
+									{
+										DayRec.AvgMovement += Rec2.AvgMovement.Value;
+										NMovement++;
+									}
+
+									if (Rec2.MinLight < DayRec.MinLight)
+									{
+										DayRec.MinLight = Rec2.MinLight;
+										DayRec.MinLightAt = Rec.MinLightAt;
+									}
+
+									if (Rec2.MaxLight < DayRec.MaxLight)
+									{
+										DayRec.MaxLight = Rec2.MaxLight;
+										DayRec.MaxLightAt = Rec.MaxLightAt;
+									}
+								}
+
+								if (NLight == 0)
+									DayRec.AvgLight = null;
+								else
+									DayRec.AvgLight /= NLight;
+
+								if (NMovement == 0)
+									DayRec.AvgMovement = null;
+								else
+									DayRec.AvgMovement /= NMovement;
+
+								await Database.Insert(DayRec);
+
+								foreach (LastDay Rec2 in await Database.Find<LastDay>(new FilterFieldLesserThan("Timestamp", Timestamp.AddDays(-100))))
+									await Database.Delete(Rec2);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Critical(ex);
+			}
+		}
+
+		/// <summary>
+		/// Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+
+			if (this.httpServer != null)
+			{
+				this.httpServer.Dispose();
+				this.httpServer = null;
+			}
+
+			if (this.sampleTimer != null)
+			{
+				this.sampleTimer.Dispose();
+				this.sampleTimer = null;
+			}
+
+			if (this.arduino != null)
+			{
+				this.arduino.digitalWrite(13, PinState.LOW);
+				this.arduino.pinMode(13, PinMode.INPUT);     // Onboard LED.
+				this.arduino.pinMode(1, PinMode.INPUT);      // Relay.
+
+				this.arduino.Dispose();
+				this.arduino = null;
+			}
+
+			if (this.arduinoUsb != null)
+			{
+				this.arduinoUsb.end();
+				this.arduinoUsb.Dispose();
+				this.arduinoUsb = null;
+			}
+
+			Log.Terminate();
+
+			deferral.Complete();
+		}
+	}
+}
